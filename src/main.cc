a561ab9db46706b5a138ece362631436bd73c934
Douglas Lamb <douglaslamb@douglaslamb.com>
2017-07-19 22:02:58 -0500

    Remove anonymous function handlers for different game states and replace them with boolean flags and conditionals.

--- src/main.cc
+++ src/main.cc
@@ -5,31 +5,21 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
-struct Place {
-  SDL_Surface* surface;
-  Place* left;
-  Place* right;
-  Place* down;
-  Place* up;
-};
-
-// Frees media and shuts down SDL 
 void close();
+SDL_Surface* loadSurface( std::string path);
 
 // Screen dimension constants
 const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 768;
 
 // global vars
-SDL_Surface* loadSurface( std::string path);
 SDL_Window* window = NULL;
 SDL_Surface* screenSurface = NULL;
 SDL_Surface* currSurface = NULL;
 
-Place* currPlace = NULL;
-
+// loadSurface returns SDL_Surface from bmp file at path.
 SDL_Surface* loadSurface( std::string path ) {
-  //The final optimized image
+  // eventual return surface
   SDL_Surface* optimizedSurface = NULL;
 
   //load image at specified path
@@ -42,27 +32,26 @@ SDL_Surface* loadSurface( std::string path ) {
     if (optimizedSurface == NULL) {
       printf("Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
     }
-    //Get rid of old loaded surface
     SDL_FreeSurface(loadedSurface);
   }
   return optimizedSurface;
 }
 
 int main( int argc, char* args[] ) {
-  // Initialize SDL
+  // init SDL
   {
     bool success = true;
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
       printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
       success = false;
     } else {
-      //Create window
+      // create window
       window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
       if( window == NULL ) {
         printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
         success = false;
       } else {
-        //Get window surface
+        // get window surface
         screenSurface = SDL_GetWindowSurface( window );
       }
     }
@@ -71,9 +60,9 @@ int main( int argc, char* args[] ) {
     }
   }
 
-  // Load media
+  // load media
   bool success = true;
-  // Load default surface
+  // load default surface
   SDL_Surface* startSurface = loadSurface( "assets/images/press.bmp" );
   if( startSurface == NULL ) {
     printf("Failed to load image.");
@@ -93,49 +82,41 @@ int main( int argc, char* args[] ) {
     return 0;
   }
 
-  // enter main loop
+  // setup main loop
   bool quit = false;
-  // set start screen surface
+  bool playing = false;
   currSurface = startSurface;
 
-  // event ref to process events
+  // placeholder to process events
   SDL_Event e;
-  // event handler function placeholder
-  // switched out depending on game state
-  std::function<void (SDL_Event)> handleEvent;
-  std::function<void (SDL_Event)> startHandler;
-  std::function<void (SDL_Event)> playingHandler;
 
-  // start screen event handler
-  startHandler = [&handleEvent, &playingHandler, &dog] (SDL_Event event) { 
-    std::cout << "starthandler ran\n";
-    if (event.type == SDL_KEYDOWN) {
-    std::cout << "starthandler got a keydown\n";
-      handleEvent = playingHandler;
-      currSurface = dog;
-    }
-  };
-
-  // playing game event handler
-  playingHandler = [&handleEvent, &startHandler, &startSurface] (SDL_Event event) { 
-    std::cout << "playinghandler ran\n";
-    if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_RIGHT) {
-      handleEvent = startHandler;
-      currSurface = startSurface;
-    }
-  };
-
-  handleEvent = startHandler;
-
+  // main loop
   while (!quit) {
     while (SDL_PollEvent(&e) != 0) {
       if (e.type == SDL_QUIT) {
         quit = true;
       } else {
-        handleEvent(e);
+        // playing loop
+        if (playing) {
+          if (e.type == SDL_KEYDOWN) {
+            if (e.key.keysym.sym == SDLK_RIGHT) {
+              currSurface = startSurface;
+              playing = false;
+            } else if (e.key.keysym.sym == SDLK_LEFT) {
+              currSurface = forest;
+            }
+          }
+        // start screen loop
+        } else {
+          if (e.type == SDL_KEYDOWN) {
+            currSurface = dog;
+            playing = true;
+          }
+        }
       }
     }
-    // blit the currSurface to the screenSurface
+
+    // blit currSurface to screenSurface
     SDL_Rect stretchRect;
     stretchRect.x = 0;
     stretchRect.y = 0;
